d7d87452637edb8a12c8315360b042b93b975730
abraham-kwizera <kwiz.abraham@gmail.com>
2022-05-26 00:47:45 +0200

    ft(configure-state): complete and delete added events on completed tasks and delete buttons wip

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React, { useState } from 'react';
-import './App.css';
-// importing components 
-import Form from './components/Form';
-import TodoList from './components/TodoList'
+import React, { useState } from "react";
+import "./App.css";
+// importing components
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 function App() {
-  const [inputText, setInputText] =useState(""); 
+  const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("All");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  
   return (
     <div className="App">
-    <header>
-      <h1>Abraham's Todo List</h1>    
-    </header>
-    
-    <Form 
-    inputText={inputText}
-    setInputText={setInputText} 
-    todos={todos} 
-    setTodos={setTodos} />
-    <TodoList/>
+      <header>
+        <h1>Abraham's Todo List</h1>
+      </header>
+
+      <Form
+        inputText={inputText}
+        setInputText={setInputText}
+        todos={todos}
+        setTodos={setTodos}
+        setStatus={setStatus}
+      />
+
+      <TodoList setTodos={setTodos} todos={todos} />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Form = ({inputText, setInputText, todos, setTodos}) =>{
+const Form = ({inputText, setInputText, todos, setTodos, setStatus}) =>{
   // JS code 
 const inputTextHandler = (e) => {
   console.log(e.target.value)
   setInputText(e.target.value)
 } 
-const submitHandler = (e) => {
+const submitTodoHandler = (e) => {
   e.preventDefault(); 
    setTodos([
      ...todos, {
@@ -17,14 +17,17 @@ const submitHandler = (e) => {
    ]);
   setInputText("");
 }
+  const statusHandler = (e) => {
+    setStatus(e.target.value); 
+  }
     return(
 <form>
       <input value={inputText} type="text" className="todo-input" onChange={inputTextHandler}/>
-      <button className="todo-button" type="submit" onClick = {submitHandler}>
+      <button className="todo-button" type="submit" onClick = {submitTodoHandler}>
         <i className="fas fa-plus-square"></i>
       </button>
       <div className="select">
-        <select name="todos" className="filter-todo">
+        <select name="todos" className="filter-todo" onChange={statusHandler}>
           <option value="all">All</option>
           <option value="completed">Completed</option>
           <option value="uncompleted">Uncompleted</option>
